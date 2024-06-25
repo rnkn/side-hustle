94450b58cec1b809afe08d0754a6662839efbc9d
Paul W. Rankin <rnkn@rnkn.xyz>
2024-06-25 20:36:59 +1000

    Use cool new rnkn.xyz email

--- side-hustle.el
+++ side-hustle.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (c) 2021-2024  Paul W. Rankin
 
-;; Author: Paul W. Rankin <hello@paulwrankin.com>
+;; Author: Paul W. Rankin <rnkn@rnkn.xyz>
 ;; Keywords: convenience
 ;; Version: 0.3.0
 ;; Package-Requires: ((emacs "24.4") (seq "2.20"))
